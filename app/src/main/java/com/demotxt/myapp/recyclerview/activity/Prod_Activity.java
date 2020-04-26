756ed5833b1c9e4028bd4b6c87ac98458a646f9d
Muhammad-Awais-418 <m.awais2211@gmail.com>
2020-04-26 21:20:33 +0500

    Some basic changes

--- app/src/main/java/com/demotxt/myapp/recyclerview/activity/Prod_Activity.java
+++ app/src/main/java/com/demotxt/myapp/recyclerview/activity/Prod_Activity.java
@@ -33,43 +33,42 @@ import java.util.Set;
 
 public class Prod_Activity extends AppCompatActivity {
 
-    private TextView tvtitle,tvdescription,tvcategory,price;
+    private TextView tvtitle, tvdescription, tvcategory, price;
     private ImageView img;
     private FloatingActionButton floatingActionButton;
-    private SharedPreferences loginpref,cartlistpref;
-    private SharedPreferences.Editor loginprefeditor,cartlistprefeditor;
+    private SharedPreferences loginpref, cartlistpref;
+    private SharedPreferences.Editor loginprefeditor, cartlistprefeditor;
     private int proid;
     public Set<String> cartids;
-
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_prod_);
-        cartids=new HashSet<String>();
+        cartids = new HashSet<String>();
 
-       loginpref=getSharedPreferences("loginpref",MODE_PRIVATE);// get login preferences which contains information like "userid" and login status
-cartlistpref=getSharedPreferences("cartprefs",MODE_PRIVATE);//get cartpreferences that contains cartitemlist
-        cartlistprefeditor=cartlistpref.edit();// this is to add stuff in preferences
+        loginpref = getSharedPreferences("loginpref", MODE_PRIVATE);// get login preferences which contains information like "userid" and login status
+        cartlistpref = getSharedPreferences("cartprefs", MODE_PRIVATE);//get cartpreferences that contains cartitemlist
+        cartlistprefeditor = cartlistpref.edit();// this is to add stuff in preferences
 
-        cartids=cartlistpref.getStringSet("cartids",cartids);//get current product ids in cartprefferences
+        cartids = cartlistpref.getStringSet("cartids", cartids);//get current product ids in cartprefferences
         //Toast.makeText(getApplicationContext(), "length:" + cartids.size(), Toast.LENGTH_SHORT).show();
         tvtitle = (TextView) findViewById(R.id.txttitle);
         tvdescription = (TextView) findViewById(R.id.txtDesc);
-       // tvcategory = (TextView) findViewById(R.id.txtCat);
+        // tvcategory = (TextView) findViewById(R.id.txtCat);
         img = (ImageView) findViewById(R.id.bookthumbnail);
-        price= (TextView)findViewById(R.id.price);
-        floatingActionButton=(FloatingActionButton)findViewById(R.id.fab);
+        price = (TextView) findViewById(R.id.price);
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
 
 
         // Recieve data
         final Intent intent = getIntent();
         String Title = intent.getExtras().getString("Title");
         String Description = intent.getExtras().getString("Description");
-        String image = intent.getExtras().getString("Thumbnail") ;
-        float pRise=intent.getExtras().getFloat("price");
-        String strprice=String.valueOf(pRise);
+        String image = intent.getExtras().getString("Thumbnail");
+        float pRise = intent.getExtras().getFloat("price");
+        String strprice = String.valueOf(pRise);
 
         // Setting values
 
@@ -77,39 +76,36 @@ cartlistpref=getSharedPreferences("cartprefs",MODE_PRIVATE);//get cartpreference
         tvtitle.setText(Title);
         tvdescription.setText(Description);
         price.setText(strprice);
-      //  img.setImageResource(image);
+        //  img.setImageResource(image);
         Picasso.get().load(image).into(img);
 
         floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-Boolean is_logedin=loginpref.getBoolean("loggedin",false);
-     if(is_logedin.equals(true))
-     {
-    proid= intent.getExtras().getInt("proid");
-    String strpid=String.valueOf(proid);
-    cartids.add(strpid);
-    cartlistprefeditor.remove("cartids");
-    cartlistprefeditor.commit();
-    cartlistprefeditor.putStringSet("cartids",cartids);
-    cartlistprefeditor.commit();
-         Toast.makeText(getApplicationContext(), "Product Added to Cart" , Toast.LENGTH_SHORT).show();
+                Boolean is_logedin = loginpref.getBoolean("loggedin", false);
+                if (is_logedin.equals(true)) {
+                    proid = intent.getExtras().getInt("proid");
+                    String strpid = String.valueOf(proid);
+                    cartids.add(strpid);
+                    cartlistprefeditor.remove("cartids");
+                    cartlistprefeditor.commit();
+                    cartlistprefeditor.putStringSet("cartids", cartids);
+                    cartlistprefeditor.commit();
+                    Toast.makeText(getApplicationContext(), "Product Added to Cart", Toast.LENGTH_SHORT).show();
 
-         //  int userid=loginpref.getInt("userid",-1);
-   //
+                    //  int userid=loginpref.getInt("userid",-1);
+                    //
 
-  //  getconnection("http://ahmedishtiaq9778-001-site1.ftempurl.com/Home/AddToCart",userid,proid);
-    }else {
-   proid= intent.getExtras().getInt("proid");
-    Intent login=new Intent(Prod_Activity.this,Login.class);
-    login.putExtra("proid",proid);
-    startActivity(login);
-
-          }
+                    //  getconnection("http://ahmedishtiaq9778-001-site1.ftempurl.com/Home/AddToCart",userid,proid);
+                } else {
+                    proid = intent.getExtras().getInt("proid");
+                    Intent login = new Intent(Prod_Activity.this, Login.class);
+                    login.putExtra("proid", proid);
+                    startActivity(login);
+                }
             }
         });
-
     }
     /* public void  getconnection(String url, final int uid, final int pid)
      {
@@ -150,5 +146,4 @@ Boolean is_logedin=loginpref.getBoolean("loggedin",false);
  };
 
     }*/
-
 }
