343da582c08f253067a2e8f90dfe8335f7b62f6d
AbrarHussain004 <63654494+AbrarHussain004@users.noreply.github.com>
2020-05-31 02:23:04 +0500

    Add Validation in payment

--- app/src/main/java/com/demotxt/myapp/recyclerview/activity/Shipping.java
+++ app/src/main/java/com/demotxt/myapp/recyclerview/activity/Shipping.java
@@ -31,12 +31,9 @@ import static com.basgeekball.awesomevalidation.ValidationStyle.BASIC;
 
 public class Shipping extends AppCompatActivity {
 
-
-    EditText t1,t2,t3,t4,t5,t6;
+    EditText t1,t2,t3,t4,t5;
     Button ship;
     AwesomeValidation awesomeValidation;
-
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,22 +42,19 @@ public class Shipping extends AppCompatActivity {
 
         awesomeValidation = new AwesomeValidation(BASIC);
 
-
-
         awesomeValidation.addValidation(Shipping.this, R.id.nameShip, "[a-zA-Z\\s]+", R.string.error_name);
-        awesomeValidation.addValidation(Shipping.this, R.id.contactShip, "^[0-9]{10}", R.string.error_contact);
         awesomeValidation.addValidation(Shipping.this, R.id.emailShip, android.util.Patterns.EMAIL_ADDRESS, R.string.error_email);
+        awesomeValidation.addValidation(Shipping.this, R.id.contactShip, "^[0-9]{10}", R.string.error_contact);
+        awesomeValidation.addValidation(Shipping.this, R.id.addShip, "^[a-zA-Z0-9_.-]*$", R.string.error_address);
 
 
-
-
-       ship=(Button)findViewById(R.id.button1);
+        ship=(Button)findViewById(R.id.button1);
         t1=(EditText) findViewById(R.id.nameShip);
         t2=(EditText) findViewById(R.id.emailShip);
         t3=(EditText) findViewById(R.id.contactShip);
         t4=(EditText) findViewById(R.id.addShip);
         t5=(EditText) findViewById(R.id.zipShip);
-        t6=(EditText) findViewById(R.id.stateShip);
+
 
 
         ship.setOnClickListener(new View.OnClickListener() {
@@ -75,7 +69,6 @@ public class Shipping extends AppCompatActivity {
                     String contact = t3.getText().toString();
                     String address = t4.getText().toString();
                     String code = t5.getText().toString();
-                    String state = t6.getText().toString();
                     Intent i = new Intent(getBaseContext(), Confirmation.class);
                     i.putExtra("getname", name);
                     i.putExtra("getaddress", address);
@@ -83,82 +76,13 @@ public class Shipping extends AppCompatActivity {
 
                 }else {
 
-
-
                     Toast.makeText(getApplicationContext(),"Invalid",Toast.LENGTH_SHORT).show();
                 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-          /*      if(isEmpty(t1)||isEmail(t2)||isEmpty(t3)||isEmpty(t4)||isEmpty(t5)||isEmpty(t6)) {
-
-                       t1.setError("Name Required");
-                       t2.setError("Email Required");
-                       t3.setError("Contact Required");
-                       t4.setError("Address Required");
-                       t5.setError("Zip Required");
-                       t6.setError("State Required");
-                   }
-                   else{
-
-
-
-
-                   }*/
 
 
             }
         });
     }
-
-
-
-   /* boolean isEmail(EditText text){
-        CharSequence email = text.getText().toString();
-        return (!TextUtils.isEmpty(email)&& Patterns.EMAIL_ADDRESS.matcher(email).matches());
-    }
-    boolean isEmpty(EditText text){
-        CharSequence str = text.getText().toString();
-        return TextUtils.isEmpty(str);
-    }
-
-    void CheckDataEntered() {
-        if(isEmpty(t1)){
-            t1.setError("Name Required");
-        }
-        if(isEmpty(t2)){
-            t2.setError("Email Required");
-
-        }
-        if(isEmpty(t3)){
-            t3.setError("Contact Required");
-
-        }
-        if(isEmpty(t4)){
-            t4.setError("Address Required");
-
-        }
-        if(isEmpty(t5)){
-            t5.setError("Zip Code Required");
-
-        }
-        if(isEmpty(t6)){
-            t6.setError("State Required");
-
-        }*/
-
-
 
 
 }
