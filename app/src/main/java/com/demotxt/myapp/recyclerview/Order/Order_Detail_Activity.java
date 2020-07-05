a09e2937bd48427a0b3e289c0233f611286a1aeb
Muhammad-Awais-418 <m.awais2211@gmail.com>
2020-07-05 21:30:34 +0500

    -Made some changes in Order Detail Activtiy -Added a recyclerView in order Detail Activity

--- app/src/main/java/com/demotxt/myapp/recyclerview/Order/Order_Detail_Activity.java
+++ app/src/main/java/com/demotxt/myapp/recyclerview/Order/Order_Detail_Activity.java
@@ -1,6 +1,9 @@
 package com.demotxt.myapp.recyclerview.Order;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,23 +12,40 @@ import android.widget.TextView;
 
 import com.demotxt.myapp.recyclerview.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order_Detail_Activity extends AppCompatActivity {
 
     TextView txt_price,txt_cart_items,txt_status,txt_id,txt_date;
     ProgressBar mProgressBar;
 
+    //For Cartlist
+    private RecyclerView mRecyclerView;
+    private Order_Detail_Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private List<Order_Detail> mList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order__detail_);
+        //Dummy Data
+        mList = new ArrayList<>();
 
+        mList.add(new Order_Detail("Shoes","15$","2",R.drawable.image_1));
+        mList.add(new Order_Detail("Shirt","25$","1",R.drawable.image_2));
+        mList.add(new Order_Detail("Tie","5$","1",R.drawable.image_3));
+        mList.add(new Order_Detail("Pant","20$","1",R.drawable.image_4));
+
+        //
         //INIT
         txt_id = findViewById(R.id.txt_id);
         txt_status = findViewById(R.id.txt_status);
         txt_price = findViewById(R.id.txt_price);
-        txt_cart_items = findViewById(R.id.txt_cart_items);
         txt_date = findViewById(R.id.txt_Date);
         mProgressBar = findViewById(R.id.progressBar);
+        mRecyclerView = findViewById(R.id.order_detail_products);
 
         //Receive Data
         Intent intent = getIntent();
@@ -51,5 +71,17 @@ public class Order_Detail_Activity extends AppCompatActivity {
             mProgressBar.setProgress(25);
         }
 
+        //
+        setadapterRecyclerView();
     }
+
+    //for Recycler View
+    private void setadapterRecyclerView() {
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new Order_Detail_Adapter(getApplicationContext(),mList);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+
 }
